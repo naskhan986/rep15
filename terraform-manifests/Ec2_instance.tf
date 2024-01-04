ebd02f31df3141d471ba57f88129e72358e56eec
Khan <nasim.khan@capgemini.com>
2024-01-05 00:47:13 +0530

    ec2-instance deploy

---
+++ terraform-manifests/Ec2_instance.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "EC2_instance" {
+  ami = "ami-01450e8988a4e7f44"
+  instance_type = "t2.small"
+  user_data = file("${path.module}/webserver.sh")
+ tags = {
+    Name = "Liza-server"
+  }
+  
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
